89307745bbcd5ae2b6e7aa44630e6cecbbde2fdb
Morris Nkosi <nkosimorris7@gmail.com>
2025-07-19 09:27:33 +0200

    Reverse words functionality

---
+++ Lightstone.Assessment/Lightstone.Assessment/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Lightstone.Assessment
+{
+    public interface IReverseWord
+    {
+        string ReversWords(string input);
+    }
+
+    public class ReversWord : IReverseWord
+    {
+        public string ReversWords(string input)
+        {
+            if (string.IsNullOrWhiteSpace((input))) return string.Empty; // return empty string for whiteSpaces
+
+            var words = input.Split(' '); // split string from the input
+
+            Array.Reverse(words); // reverse words
+
+            return string.Join(' ', words);
+
+            /**  var reverseInput = ""; another solution that can work
+          
+              for (var i = words.Length - 1; i >= 0; i--)
+              {
+                  reverseInput += words[i];
+                  if (i > 0) reverseInput += " "; // adding spaces between the words
+              }
+
+              return reverseInput; **/
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
